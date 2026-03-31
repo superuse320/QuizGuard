6c2a3012aa98753385e92c2990827b8a15d36be7
unknown <castroobed53@gmail.com>
2026-03-31 17:36:01 -0400

    chore: Configure CORS middleware with specific options

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.options('*', cors())
 app.use(express.json())
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
